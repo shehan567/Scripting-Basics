c3f5f5bdcd60784caef38447aa07240224b9cef1
shehan567 <hushhwsply@gmail.com>
2020-08-27 03:26:36 -0400

    Just a try

---
+++ 03-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# If we name to some data then that name is called a variable
+
+# When it comes to data we have different types, 1. Numbers, 2. Floatind Values, 3. Characters, 4. Strings, 5. Booleans
+
+a=10
+b=xyz
+c=true
+d=9.9999
+
+# Shell by default does not support data types. Everything is a string. 
+
+# Access variable :  ${a} or $a, In some scenarios we use {} see line 17
+
+echo -e "/e[33m$a\e[0m"
