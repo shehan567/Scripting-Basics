72ea2d9597d0cf70b08119a81f3dca497237ef4e
shehan567 <hushhwsply@gmail.com>
2020-08-27 16:28:01 -0400

    Another try

--- 05-input-arguments.sh
+++ 05-input-arguments.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Special variables with regards to inputs, $0 - $n, $@, $#
-$1 = Shehan
-$2 = date
+
 
 
 echo script name = $0
-echo First Argument = $1
-echo Second Argument = $2
+echo uname = $1
+echo pwd = $2
 
