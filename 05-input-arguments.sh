de30d4f5e76075cffa36b5ebd4f5369e62ca9fe4
shehan567 <hushhwsply@gmail.com>
2020-08-28 11:55:38 -0400

    Added echo all arguments & Number of arguments

--- 05-input-arguments.sh
+++ 05-input-arguments.sh
@@ -8,3 +8,11 @@ echo script name = $0
 echo first Argument= $1
 echo Second Argument = $2
 
+### Beyond 9, From 10 argument ${}
+
+echo All arguments = $*
+echo All arguments = $@
+
+echo NUmberf of Arguments = $#
+
+
