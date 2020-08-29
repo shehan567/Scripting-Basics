4612867dfa09aec84df29b9dda8c22f6b4e9c80b
shehan567 <hushhwsply@gmail.com>
2020-08-29 14:29:22 -0400

    Exit sample function and main program added

---
+++ exitstatus.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# Exit status rangers from 0-255
+# Success - 0
+# 1-125 - Faliure, Partial failure
+
+# exit 0
+
+# User is suggested to use values only from 1-125, Because beyond system uses those numbers
+
+
+sample () {
+    echo hello
+    echo first argument = $1
+    return 20
+    echo bye 
+}
+
+
+sample abo
+
+echo Exit status of Sample Function = $?
+
+
