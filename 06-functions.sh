eae0609731d8168cb553cc1c77402f5c333926ec
shehan567 <hushhwsply@gmail.com>
2020-08-29 11:20:08 -0400

    Added Observations

--- 06-functions.sh
+++ 06-functions.sh
@@ -5,8 +5,9 @@
 # Declare function
 
 sample () {
+    a=30
     echo Hello World from Function
-    echo a = ${a}
+    echo a in Function = ${a}
     b=20 
 }
 
@@ -15,10 +16,12 @@ sample () {
 # Main Programm
 a=10
 sample
-echo b = ${b}
+echo a in Main Program = ${a}
+echo b in Main Program = ${b}
+
 
 # Observations
 
 # 1. Functions are always before main program
 # 2. Variables in Main Program can be accessed in funcion & Vise-Versa
-# 
+# 3. Variables from Main Program can be overwritten by function & Vice-Versa
