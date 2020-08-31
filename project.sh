34bfb2bb4fcee912778ae65a617404896c0ace34
shehan567 <hushhwsply@gmail.com>
2020-08-31 02:32:41 -0400

    Added missing [ in USAGE function

--- project.sh
+++ project.sh
@@ -67,8 +67,8 @@ user () {
 }
 
 USAGE() {
-    echo -e "Usage\t\t\t : $0 1;4;<Choose component from below list with the script>"
-    echo -e "Components\t\t : \e[32mfrontend \e33mmongodb \e34mredis \e35mmysql \e36mrabbitmq \e34mcart \e32mcatalogue \e33shipping \e34payment \e35user"
+    echo -e "Usage\t\t\t : $0 \e[1;4;<Choose component from below list with the script>"
+    echo -e "Components\t\t : \e[32mfrontend \e[33mmongodb \e[34mredis \e[35mmysql \e[36mrabbitmq \e[34mcart \e[32mcatalogue \e33[shipping \e34[payment \e[35user\e[0m"
     echo -e "For all components use\t : all"
     exit 1
 }
